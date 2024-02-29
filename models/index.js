bf248f2fa9e61ee8e83abd78a577551efc826872
monsosam <monsonsamuel5@gmail.com>
2024-02-28 19:30:10 -0700

    tiny edit

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User.js');
-const Post = require('./Post.js');
-const Comment = require('./Comment.js');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
